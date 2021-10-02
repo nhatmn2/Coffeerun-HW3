d2541ceed16b2efa0dbad4cc716d6819ddb5b107
nhatmn2 <nhatmn2@csu.fullerton.edu>
2021-10-01 21:37:33 -0400

    new version of index.html and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,18 @@
 (function (window) {
     'use strict';
+    var FORM_SELECTOR = '[data-coffee-order="form"]';
+
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
+    var FormHandler = App.FormHandler;
+
     var truck = new Truck('ncc-1701', new DataStore());
     window.truck = truck;
+
+    var formHandler = new FormHandler(FORM_SELECTOR);
+    formHandler.addSubmitHandler(truck.createOrder.bind(truck));
+
+    console.log(formHandler);
+    
 })(window);
